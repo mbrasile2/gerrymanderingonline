b93b55d0d66197f2abfe9e52d48da0bc5c5c3c16
Bongsung Bae <bongsung.bae@stonybrook.edu>
2018-03-20 09:41:43 -0700

    Account class 2

--- src/main/java/jaguars/user/AccountController.java
+++ src/main/java/jaguars/user/AccountController.java
@@ -1,24 +1,32 @@
 package jaguars.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class AccountController {
     @Autowired
     AccountRepository accountRepository;
 
-    @RequestMapping("/account")
-    public void addPerson(@RequestBody Account account) {
+    @RequestMapping(value="/accounts", method = RequestMethod.POST)
+    public void postPerson(@RequestBody Account account) {
         accountRepository.save(account);
     }
 
-    @RequestMapping("account/{id}")
+    @RequestMapping(value="/accounts/{id}", method = RequestMethod.GET)
     public Account getPerson(@PathVariable String id) {
         return accountRepository.findOne(id);
     }
 
+    @RequestMapping(value="/accounts", method = RequestMethod.GET)
+    public List<Account> getAllAccounts() {
+        return (List<Account>) accountRepository.findAll();
+    }
+
+    @RequestMapping(value = "/accounts/{id}", method = RequestMethod.DELETE)
+    public void deletePerson(@PathVariable String id) {
+        accountRepository.delete(id);
+    }
 }
--- src/main/java/jaguars/user/AccountRepository.java
+++ src/main/java/jaguars/user/AccountRepository.java
@@ -2,5 +2,7 @@ package jaguars.user;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface AccountRepository extends CrudRepository<Account, String> {
 }
